caad42bb06146334a4f154e431706bf719130d0b
SergeyMell <sergey.mell@agilie.com>
2016-08-06 22:13:07 +0300

    Implemented simple messages adding

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,9 +4,14 @@ class MessagesController < ApplicationController
 
   def index
     @user_name = $redis.get(session[:session_id])
+    @messages = $redis.lrange('messages', -10, -1)
   end
 
   def create
+    permit_params = params.require(:message).permit(:text)
+    message_data = {user: $redis.get(session[:session_id]), text: permit_params[:text]}
 
+    $redis.rpush('messages', message_data.to_json)
+    redirect_to messages_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,6 @@ class UsersController < ApplicationController
 
   def create
     user_data = params.require(:user).permit(:name)
-    puts 'params.inspect'
-    puts params.inspect
     $redis.set(session[:session_id], user_data[:name], ex: 1.hour)
     redirect_to root_path
   end
