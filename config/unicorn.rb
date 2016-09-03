34246c1fc216a3aef8f9393efc13da45eaef1407
SergeyMell <sergey.mell@agilie.com>
2016-09-04 00:07:28 +0300

    Configured deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,8 +2,8 @@ deploy_to  = "/var/www/myapp"
 rails_root = "#{deploy_to}/current"
 pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
 socket_file= "#{deploy_to}/shared/unicorn.sock"
-log_file   = "#{rails_root}/log/unicorn.log"
-err_log    = "#{rails_root}/log/unicorn_error.log"
+log_file   = "#{rails_root}/unicorn.log"
+err_log    = "#{rails_root}/unicorn_error.log"
 old_pid    = pid_file + '.oldbin'
 
 timeout 30
